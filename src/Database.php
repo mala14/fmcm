34408a117b9428652b23e63fd04eddab3d4c1085
mala14 <8696166+mala14@users.noreply.github.com>
2021-08-18 11:42:58 +0200

    Add files via upload

---
+++ src/Database.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* db connection
+*/
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$servername;dbname=$database;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $username, $password, $opt);
+} catch (PDOExeption $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
